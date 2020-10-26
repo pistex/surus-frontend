11abc61357fb408de7983a8695b816cd3d90e763
Panithi Nakkhruea <panithi.nakkhruea@protonmail.com>
2020-10-26 20:39:21 +0700

    - Add language switcher to homepage. - Add cloudbuild.yaml - pre-production

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,32 @@
 <template>
   <v-container id="home">
     <v-card dark>
-      <v-card-title :class="$vuetify.breakpoint.smAndDown ? 'text-h4' : 'text-h2'">
-        Featured articles
+      <v-card-title
+        :class="$vuetify.breakpoint.smAndDown ? 'text-h4' : 'text-h2'"
+      >
+        {{ isPrimaryLanguage ? 'Featured articles': 'บทความแนะนำ' }}
+        <v-spacer />
+        <v-btn @click="isPrimaryLanguageBuffer = !isPrimaryLanguageBuffer">
+          {{ isPrimaryLanguage ? 'EN': 'TH' }}
+        </v-btn>
       </v-card-title>
       <v-carousel cycle hide-delimiters show-arrows-on-hover height="400">
         <v-carousel-item
-          v-for="blog in allBlogs.filter(blog => {return blog.is_featured})"
+          v-for="blog in allBlogs.filter((blog) => {
+            return blog.is_featured;
+          })"
           :key="blog.id"
           gradient="to top right, rgba(0,0,0,.4), rgba(0,0,0,1)"
           :to="`/blog/${blog.slug}/`"
-          :src="allImages.length > 0 ? allImages.find((object) => object.id == blog.thumbnail).image : ''"
+          :src="
+            allImages.length > 0
+              ? allImages.find((object) => object.id == blog.thumbnail).image
+              : ''
+          "
         >
           <!-- set the condition allImages.length > 0 to prevent error. -->
-          <v-container
-            fill-height
-            class="align-end headline"
-          >
-            {{ blog.title.en }}
+          <v-container fill-height class="align-end headline">
+            {{ isPrimaryLanguage ? blog.title.en : blog.title.th }}
           </v-container>
         </v-carousel-item>
       </v-carousel>
@@ -28,9 +37,16 @@
 <script>
 import { mapGetters, mapActions } from 'vuex'
 export default {
-  components: {
+  components: {},
+  data () {
+    return {
+      isPrimaryLanguageBuffer: true
+    }
   },
   computed: {
+    isPrimaryLanguage () {
+      return this.isPrimaryLanguageBuffer
+    },
     ...mapGetters('blogStore', ['allBlogs', 'allImages'])
   },
   created () {
